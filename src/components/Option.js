17f306408238a57f89859fd4b43182698fbc4a8c
Kevin Suriel <mrkevinsuriel@gmail.com>
2021-01-28 19:38:54 -0500

    Create a button that responsd when a click is used on remove

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,15 +1,28 @@
-import React, {useState} from 'react';
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
 
-
-function Option(props){
-    return(
-        <div>
-        
-          {props.option && props.option.map(op=>{
-              return <p key={op}>{op}</p>
-          })}
-        </div>
-        
-    )
+function Option(props) {
+  const handleDelete = () => {
+    console.log("Works");
+  };
+  return (
+    <div>
+      {props.option &&
+        props.option.map((op) => {
+          return (
+            <div>
+              {op}
+              <Button
+                onClick={handleDelete}
+                variant="contained"
+                color="primary"
+              >
+                Remove
+              </Button>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 export default Option;
