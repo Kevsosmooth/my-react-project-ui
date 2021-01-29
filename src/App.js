c2c084c6b77f96458a63d190b347d4b5db8c528d
Kevin Suriel <mrkevinsuriel@gmail.com>
2021-01-28 19:24:00 -0500

    Working Version of Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-
-import React, {useState} from 'react'
-import FormField from './components/FormField'
-import Header from './components/Header'
+import React, { useState } from "react";
+import FormField from "./components/FormField";
+import Header from "./components/Header";
 import Container from "@material-ui/core/Container";
-import Option from './components/Option'
-function App(){
+import Option from "./components/Option";
+function App() {
+  const [option, setOption] = useState([]);
 
-
-  const state = {
-    option:['TEST','TEST2'],
-  }
-
-  return(
-  <div>
-    <Container maxWidth='md'>
-    <Header/>
-    <Option option={state.option} />
-    <FormField option={state.option}/>
-    </Container>
-  </div>
-
-  )
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Header />
+        <Option option={option} />
+        <FormField setOption={setOption} option={option} />
+      </Container>
+    </div>
+  );
 }
 export default App;
--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,29 +1,34 @@
-import React, {useState} from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
+function FormField(props) {
+  // const handleAddOption = () =>{
+  //     this.setState((prevProp)=>{
+  //         return props.prevProp.concat(field)
+  //     })
+  // }
+  //The above function is not working as expected
 
-function FormField(props){
-
-    
-    // const handleAddOption = () =>{
-    //     this.setState((prevProp)=>{
-    //         return props.prevProp.concat(field)
-    //     })
-    // }
-    //The above function is not working as expected
-
-    const [field, setTextField] = useState('');
-    return(
-        
-        
-        <div>
-            <TextField value={field} onChange={(e) => setTextField(e.target.value)} id="outlined-basic" label="Options" variant="outlined"/>
-            <Button variant="contained" color="primary">Add Option</Button>
-
-           
-        </div> 
-    );
-};
+  const [field, setTextField] = useState("");
+  const handleClick = () => {
+    props.setOption((prevArray) => [...prevArray, field]);
+    setTextField("");
+  };
+  return (
+    <div>
+      <TextField
+        value={field}
+        onChange={(e) => setTextField(e.target.value)}
+        id="outlined-basic"
+        label="Options"
+        variant="outlined"
+      />
+      <Button onClick={handleClick} variant="contained" color="primary">
+        Add Option
+      </Button>
+    </div>
+  );
+}
 
 export default FormField;
